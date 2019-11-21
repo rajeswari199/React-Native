84360956000ed1ad63b7ab0c13834ec30a49d4a1
MurugalakshmiM <mmurugalakshmi.03@gmail.com>
2019-11-21 11:46:50 +0530

    navigation activated

--- todo-app/App.js
+++ todo-app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 // import TodoForm from './components/todo-form'
 // import TodoList from './components/todo-list'
+import Login from './components/login'
 import RainTerrace from './components/rain-terrace'
 import { Provider } from 'react-redux';
 import store from './store';
@@ -11,10 +12,10 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <RainTerrace />
+        <Navigation />
       </Provider >
     );
   }
 }
 
-export default App;
+export default App
